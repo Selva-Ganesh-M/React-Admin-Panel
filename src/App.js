4afa0637ed9de1c0a6ea672f5e4497cf5a1bc441
Selva Ganesh M <selvadev2k@gmail.com>
2022-11-29 14:53:54 +0530

    create the sticky sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container">
+        <Sidebar />
+        <div className="other">Other</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,97 @@
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+import "./sidebar.scss";
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <section className="sidebar-section">
+        <h3 className="sidebar-title">Dashboard</h3>
+        <ul className="sidebar-list">
+          <li className="sidebar-li" active>
+            <LineStyle />
+            <span>Home</span>
+          </li>
+          <li className="sidebar-li">
+            <Timeline />
+            <span>Analytics</span>
+          </li>
+          <li className="sidebar-li">
+            <TrendingUp />
+            <span>Sales</span>
+          </li>
+        </ul>
+      </section>
+      <section className="sidebar-section">
+        <h3 className="sidebar-title">Quick Menu</h3>
+        <ul className="sidebar-list">
+          <li className="sidebar-li" active>
+            <PermIdentity />
+            <span>User</span>
+          </li>
+          <li className="sidebar-li">
+            <Storefront />
+            <span>Products</span>
+          </li>
+          <li className="sidebar-li">
+            <AttachMoney />
+            <span>Transanctions</span>
+          </li>
+          <li className="sidebar-li">
+            <BarChart />
+            <span>Reports</span>
+          </li>
+        </ul>
+      </section>
+      <section className="sidebar-section">
+        <h3 className="sidebar-title">Notifications</h3>
+        <ul className="sidebar-list">
+          <li className="sidebar-li" active>
+            <MailOutline />
+            <span>Mail</span>
+          </li>
+          <li className="sidebar-li">
+            <DynamicFeed />
+            <span>Feedback</span>
+          </li>
+          <li className="sidebar-li">
+            <ChatBubbleOutline />
+            <span>Messages</span>
+          </li>
+        </ul>
+      </section>
+      <section className="sidebar-section">
+        <h3 className="sidebar-title">Staff</h3>
+        <ul className="sidebar-list">
+          <li className="sidebar-li" active>
+            <WorkOutline />
+            <span>Manage</span>
+          </li>
+          <li className="sidebar-li">
+            <Timeline />
+            <span>Analytics</span>
+          </li>
+          <li className="sidebar-li">
+            <Report />
+            <span>Reports</span>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Sidebar;
