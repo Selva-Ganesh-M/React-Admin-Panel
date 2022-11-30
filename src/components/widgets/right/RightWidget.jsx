290ff479a1a39941893c3cc209a7a05bd1a01f03
Selva Ganesh M <selvadev2k@gmail.com>
2022-11-30 13:01:47 +0530

    complete widgets

--- src/components/widgets/right/RightWidget.jsx
+++ src/components/widgets/right/RightWidget.jsx
@@ -4,14 +4,14 @@ const RightWidget = () => {
   const TableRow = ({ image, name, status, date, amount }) => {
     return (
       <tr className="tb-tr">
-        <td>
+        <td className="img-div">
           <img src={image} className="img" alt="user-image" />
         </td>
         <td className="name">{name}</td>
         <td className="date">{date}</td>
         <td className="amount">$ {amount}</td>
         <td className="status">
-          <button className={status}>{status}</button>
+          <button className={`btn ${status}`}>{status}</button>
         </td>
       </tr>
     );
@@ -37,6 +37,42 @@ const RightWidget = () => {
               }
               name={"Eren Yeager"}
               status={"Approved"}
+              date={"10.11.2022"}
+              amount={1200}
+            />
+            <TableRow
+              image={
+                "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              }
+              name={"Eren Yeager"}
+              status={"Pending"}
+              date={"10.11.2022"}
+              amount={100}
+            />
+            <TableRow
+              image={
+                "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              }
+              name={"Eren Yeager"}
+              status={"Pending"}
+              date={"10.11.2022"}
+              amount={1200}
+            />
+            <TableRow
+              image={
+                "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              }
+              name={"Eren Yeager"}
+              status={"Approved"}
+              date={"10.11.2022"}
+              amount={7800}
+            />
+            <TableRow
+              image={
+                "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              }
+              name={"Eren Yeager"}
+              status={"Rejected"}
               date={"10.11.2022"}
               amount={1200}
             />
